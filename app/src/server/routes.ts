bb5a88423cec9d67814b5a55fb5d8d3de26bb7f0
Alshady <ameen.alkaisi01@gmail.com>
2022-06-05 19:46:38 -0400

    changed port and added post-receive script to symlink to into a bare git repo

--- app/src/server/routes.ts
+++ app/src/server/routes.ts
@@ -3,9 +3,11 @@ import * as express from 'express';
 const router = express.Router();
 const path = require('path');
 
+/*
 router.get('/api/hello', (_req, res, _next) => {
     res.json('World');
 });
+*/
 
 router.get('/*', (_req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
