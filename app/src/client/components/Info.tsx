ce1798f7317d04a0843f3f048a4338241eaf2b1e
Alshady <ameen.alkaisi01@gmail.com>
2022-07-14 14:29:56 -0400

    added more information in the info section

--- app/src/client/components/Info.tsx
+++ app/src/client/components/Info.tsx
@@ -4,10 +4,14 @@ const Info: React.FC<{}> = (): React.ReactElement => {
 	return (
 		<div className="info">
 			<h1>Instructions...</h1>
-			<p>More instructions here</p>
+			<p>The syntax looks like this:</p>
+			<p>EXPRESSION ::= NODE DASH GREATER_THAN LBRACKET WORD RBRACKET NODE_LIST</p>
+			<p>NODE_LIST ::= NODE NODE_LIST | &lt; nothing &gt;</p>
+			<p>NODE ::= WORD | WORD LBRACKET NODE_TYPE RRACKET</p>
+			<p>NODE_TYPE ::= s | f | sf | fs </p>
+			<p>WORD ::= [a-zA-Z0-9_]+</p>
 
-			<h1>What are NFAs or DFAs?</h1>
-			<p>More info</p>
+			<h1><a href="https://www.geeksforgeeks.org/introduction-of-finite-automata/">What are NFAs or DFAs?</a></h1>
 		</div>
 	);
 }
