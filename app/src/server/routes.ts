ff0e9ba88b87e848b7799fcf9e4f7ec026059570
Alshady <ameen.alkaisi01@gmail.com>
2022-08-02 11:07:56 -0400

    added testing end-point

--- app/src/server/routes.ts
+++ app/src/server/routes.ts
@@ -3,6 +3,10 @@ import * as path from 'path';
 
 const router = express.Router();
 
+router.get('/test', (_req, res) => {
+	res.send("server is reachable");
+});
+
 router.get('/*', (_req, res) => {
 	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
