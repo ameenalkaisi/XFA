7f737f6990340884c3b7d5603fe03a86d62aa05a
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-01-25 19:16:41 -0500

    changed copyright notice's year from 2022 to 2023

--- app/src/client/components/Footer.tsx
+++ app/src/client/components/Footer.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 const Footer: React.FC<{}> = (): React.ReactElement => {
 	return (
 		<footer className="bg-primary text-center text-lg-start">
-			<p className="text-center">&copy; 2022 Ameen Al-Kaisi. All rights reserved.</p>
+			<p className="text-center">&copy; 2023 Ameen Al-Kaisi. All rights reserved.</p>
 			<p className="text-center">
 				<small className="muted">This site is a WIP</small>
 			</p>
